c335c1ed7cb939d6eb7c5dbb4b8a9c96fb1e05e3
AhmedHussien <Ahmed.hs9090@gmail.com>
2023-01-17 23:05:33 +0200

    added employees Mapping

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,21 +1,35 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping("/employee")
-    public String getEmployees(){
-        return "employees";
+    @Autowired
+    private EmployeesService EmployeeService;
+
+    @GetMapping("/employees")
+    public ResponseEntity<?> getEmployees() {
+        return new ResponseEntity<>(EmployeeService.getAllEmployees(), HttpStatus.OK);
     }
 
-
-    @PostMapping("/employeebody/")
-    public String getEmployees(@RequestHeader("Content-Type") String ContentType){
-        return ContentType;
+    @PostMapping("/employees/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable("id") String id) {
+        return new ResponseEntity<>(EmployeeService.getEmployeeById(id), HttpStatus.OK);
     }
+
+    @PostMapping("/employees-by-jobid/{id}")
+    public ResponseEntity<?> getEmployeeByJobId(@PathVariable("id") String id) {
+        return new ResponseEntity<>(EmployeeService.getEmployeeByJobId(id), HttpStatus.OK);
+    }
+
 
 }
